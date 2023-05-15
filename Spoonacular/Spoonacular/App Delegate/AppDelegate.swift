20f866a8b884ea487f128cb7b38c5c9792218258
Giancarlo Castañeda <castag.gian@gmail.com>
2023-05-15 14:46:05 -0500

    build(app): re organice main folders

--- Spoonacular/Spoonacular/App Delegate/AppDelegate.swift
+++ Spoonacular/Spoonacular/App Delegate/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  Spoonacular
-//
-//  Created by Giancarlo Castañeda Garcia on 15/05/23.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
