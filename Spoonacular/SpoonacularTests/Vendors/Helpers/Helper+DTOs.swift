70e083cd82eecf5438f83cd6a612840b04c70a61
Giancarlo Castañeda <castag.gian@gmail.com>
2023-05-17 01:28:49 -0500

    test(api): add RecipesInformationDTOTests

--- Spoonacular/SpoonacularTests/Vendors/Helpers/Helper+DTOs.swift
+++ Spoonacular/SpoonacularTests/Vendors/Helpers/Helper+DTOs.swift
@@ -10,9 +10,21 @@ extension Helper {
         return try XCTUnwrap(NSDataAsset(name: "Recipes_ComplexSearch_GET.json", bundle: bundle)).data
     }
 
+    static func makeRecipesInformationDTOData() throws -> Data {
+        let bundle = try XCTUnwrap(Bundle(identifier: "co.gian.Spoonacular"))
+
+        return try XCTUnwrap(NSDataAsset(name: "Recipes_Information_GET.json", bundle: bundle)).data
+    }
+
     static func makeRecipesComplexSearchDTO() throws -> RecipesComplexSearchDTO {
         let jsonDecoder = makeJSONDecoder()
 
         return try jsonDecoder.decode(RecipesComplexSearchDTO.self, from: makeRecipesComplexSearchDTOData())
+    }
+
+    static func makeRecipesInformationDTO() throws -> RecipesInformationDTO {
+        let jsonDecoder = makeJSONDecoder()
+
+        return try jsonDecoder.decode(RecipesInformationDTO.self, from: makeRecipesInformationDTOData())
     }
 }
